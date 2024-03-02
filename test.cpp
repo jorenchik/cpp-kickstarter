c5f6ba9a5d618dfd7bbb82faaa0f62252967f5b6
jorenchik <jorens.stekels@gmail.com>
2024-03-02 10:51:29 +0200

    change to google test

--- test.cpp
+++ test.cpp
@@ -1,28 +1,3 @@
-// CPlusPlusUnit Usage Example.
-// CPlusPlusUnit - C++ Unit testing TDD framework (github.com/cppunit/cppunit)
-#include "MyLibrary.h"
-#include "cppunit.h"
-#include "main_lib.h"
+#include <gtest/gtest.h>
 
-// Class under test.
-class test_class {
-  public:
-    int calculate() {
-        int n, m;
-        std::cin >> n >> m;
-        return n + m;
-    }
-};
-
-// Test examples.
-class Cppunit_tests : public Cppunit {
-    void single_test() {
-        CHECKT(add(2, 3) == 5);
-        CHECKT(functionToTest(5) == 10);
-    }
-};
-
-int main(int argc, char *argv[]) {
-    // Test invocation example.
-    return (new Cppunit_tests)->run();
-}
+TEST(TestLib, myLibTest) { EXPECT_EQ("a", "c"); }
